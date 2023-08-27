f95e2b54493574ebe5e6411c221043eec1db4dc3
Conga.Keovast.Eldest <mcry416@outlook.com>
2023-08-27 18:21:48 +0800

    add setImage fucntion.

---
+++ Sources/AsyncDrawKit/AsyncDraw/AsyncImageView+Extension/AsyncImageView+SetImage.swift
@@ -0,0 +1,49 @@
+//
+//  AsyncImageView+SetImage.swift
+//  Compent
+//
+//  Created by Eldest's MacBook on 2023/8/27.
+//
+
+import UIKit
+
+extension AsyncImageView {
+    
+    private static var sg_asyncImageCaches: AsyncImageViewCache = { AsyncImageViewCache(limitCount: 100) }()
+    
+    /**
+     Set image with an URL sting, provide placeholder and completion callback.
+     - Parameter urlString: Image url string.
+     - Parameter placeholder: When the image was not loaded, and show placeholder from local resource.
+     - Parameter completion: Image loaded succeed, and execute this callback.
+     */
+    func sg_setImage(_ urlString: String, placeholderName placeholder: String? = nil, completion: ((UIImage) -> Void)? = nil) {
+        if let cache = AsyncImageView.sg_asyncImageCaches.getObject(forKey: urlString) as? UIImage {
+            self.image = cache
+            return
+        }
+        
+        if let placeholder = placeholder, let placeholderImage = UIImage(named: placeholder) {
+            self.image = placeholderImage
+        }
+        
+        DispatchQueue.global().async {
+            guard let url = URL(string: urlString) else { return }
+            URLSession.shared.dataTask(with: url) { data, response, error  in
+                if let data = data, let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        AsyncImageView.sg_asyncImageCaches.setObject(image, forKey: urlString)
+                        self.image = image
+                        completion?(image)
+                    }
+                } else {
+                    if let error = error {
+                        debugPrint("----> ERROR : \(error)")
+                    }
+                }
+            }.resume()
+        }
+        
+    }
+    
+}
---
+++ Sources/AsyncDrawKit/AsyncDraw/AsyncImageView+Extension/LRUCache.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Eldest's MacBook on 2023/8/27.
+//
+
+import Foundation
